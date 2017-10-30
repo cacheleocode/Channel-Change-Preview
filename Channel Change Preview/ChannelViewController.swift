f1d6e39ccd59ac38c5503a5cf9d143fc1ee65a37
Leo Dominguez <Leo@LeoDominguez.com>
2017-10-30 15:09:34 -0700

    new icon

---
+++ Channel Change Preview/ChannelViewController.swift
@@ -0,0 +1,188 @@
+//
+//  ChannelViewController.swift
+//  Channel Change Preview
+//
+//  Created by DOMINGUEZ, LEO on 10/30/17.
+//  Copyright © 2017 DOMINGUEZ, LEO. All rights reserved.
+//
+
+import UIKit
+
+class ChannelViewController: UIViewController {
+    @IBOutlet weak var loadingbarView: UIImageView!
+    
+    // loading bar
+    
+    var loading_0: UIImage!
+    var loading_1: UIImage!
+    var loading_2: UIImage!
+    var loading_3: UIImage!
+    var loading_4: UIImage!
+    var loading_5: UIImage!
+    var loading_6: UIImage!
+    var loading_7: UIImage!
+    var loading_8: UIImage!
+    var loading_9: UIImage!
+    var loading_10: UIImage!
+    var loading_11: UIImage!
+    var loading_12: UIImage!
+    var loading_13: UIImage!
+    var loading_14: UIImage!
+    var loading_15: UIImage!
+    var loading_16: UIImage!
+    var loading_17: UIImage!
+    var loading_18: UIImage!
+    var loading_19: UIImage!
+    var loading_20: UIImage!
+    var loading_21: UIImage!
+    var loading_22: UIImage!
+    var loading_23: UIImage!
+    var loading_24: UIImage!
+    var loading_25: UIImage!
+    var loading_26: UIImage!
+    var loading_27: UIImage!
+    var loading_28: UIImage!
+    var loading_29: UIImage!
+    var loading_30: UIImage!
+    var loading_31: UIImage!
+    var loading_32: UIImage!
+    var loading_33: UIImage!
+    var loading_34: UIImage!
+    var loading_35: UIImage!
+    var loading_36: UIImage!
+    var loading_37: UIImage!
+    var loading_38: UIImage!
+    var loading_39: UIImage!
+    var loading_40: UIImage!
+    var loading_41: UIImage!
+    var loading_42: UIImage!
+    var loading_43: UIImage!
+    var loading_44: UIImage!
+    var loading_45: UIImage!
+    var images: [UIImage]!
+    var animatedImage: UIImage!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // loading bar
+        
+        loading_0 = UIImage(named: "videoloadingbar_loop_00000")
+        loading_1 = UIImage(named: "videoloadingbar_loop_00001")
+        loading_2 = UIImage(named: "videoloadingbar_loop_00002")
+        loading_3 = UIImage(named: "videoloadingbar_loop_00003")
+        loading_4 = UIImage(named: "videoloadingbar_loop_00004")
+        loading_5 = UIImage(named: "videoloadingbar_loop_00005")
+        loading_6 = UIImage(named: "videoloadingbar_loop_00006")
+        loading_7 = UIImage(named: "videoloadingbar_loop_00007")
+        loading_8 = UIImage(named: "videoloadingbar_loop_00008")
+        loading_9 = UIImage(named: "videoloadingbar_loop_00009")
+        loading_10 = UIImage(named: "videoloadingbar_loop_00010")
+        loading_11 = UIImage(named: "videoloadingbar_loop_00011")
+        loading_12 = UIImage(named: "videoloadingbar_loop_00012")
+        loading_13 = UIImage(named: "videoloadingbar_loop_00013")
+        loading_14 = UIImage(named: "videoloadingbar_loop_00014")
+        loading_15 = UIImage(named: "videoloadingbar_loop_00015")
+        loading_16 = UIImage(named: "videoloadingbar_loop_00016")
+        loading_17 = UIImage(named: "videoloadingbar_loop_00017")
+        loading_18 = UIImage(named: "videoloadingbar_loop_00018")
+        loading_19 = UIImage(named: "videoloadingbar_loop_00019")
+        loading_20 = UIImage(named: "videoloadingbar_loop_00020")
+        loading_21 = UIImage(named: "videoloadingbar_loop_00021")
+        loading_22 = UIImage(named: "videoloadingbar_loop_00022")
+        loading_23 = UIImage(named: "videoloadingbar_loop_00023")
+        loading_24 = UIImage(named: "videoloadingbar_loop_00024")
+        loading_25 = UIImage(named: "videoloadingbar_loop_00025")
+        loading_26 = UIImage(named: "videoloadingbar_loop_00026")
+        loading_27 = UIImage(named: "videoloadingbar_loop_00027")
+        loading_28 = UIImage(named: "videoloadingbar_loop_00028")
+        loading_29 = UIImage(named: "videoloadingbar_loop_00029")
+        loading_30 = UIImage(named: "videoloadingbar_loop_00030")
+        loading_31 = UIImage(named: "videoloadingbar_loop_00031")
+        loading_32 = UIImage(named: "videoloadingbar_loop_00032")
+        loading_33 = UIImage(named: "videoloadingbar_loop_00033")
+        loading_34 = UIImage(named: "videoloadingbar_loop_00034")
+        loading_35 = UIImage(named: "videoloadingbar_loop_00035")
+        loading_36 = UIImage(named: "videoloadingbar_loop_00036")
+        loading_37 = UIImage(named: "videoloadingbar_loop_00037")
+        loading_38 = UIImage(named: "videoloadingbar_loop_00038")
+        loading_39 = UIImage(named: "videoloadingbar_loop_00039")
+        loading_40 = UIImage(named: "videoloadingbar_loop_00040")
+        loading_41 = UIImage(named: "videoloadingbar_loop_00041")
+        loading_42 = UIImage(named: "videoloadingbar_loop_00042")
+        loading_43 = UIImage(named: "videoloadingbar_loop_00043")
+        loading_44 = UIImage(named: "videoloadingbar_loop_00044")
+        loading_45 = UIImage(named: "videoloadingbar_loop_00045")
+        
+        images = [
+            loading_0,
+            loading_1,
+            loading_2,
+            loading_3,
+            loading_4,
+            loading_5,
+            loading_6,
+            loading_7,
+            loading_8,
+            loading_9,
+            loading_10,
+            loading_11,
+            loading_12,
+            loading_13,
+            loading_14,
+            loading_15,
+            loading_16,
+            loading_17,
+            loading_18,
+            loading_19,
+            loading_20,
+            loading_21,
+            loading_22,
+            loading_23,
+            loading_24,
+            loading_25,
+            loading_26,
+            loading_27,
+            loading_28,
+            loading_29,
+            loading_30,
+            loading_31,
+            loading_32,
+            loading_33,
+            loading_34,
+            loading_35,
+            loading_36,
+            loading_37,
+            loading_38,
+            loading_39,
+            loading_40,
+            loading_41,
+            loading_42,
+            loading_43,
+            loading_44,
+            loading_45
+        ]
+        
+        animatedImage = UIImage.animatedImage(with: images, duration: 1.3)
+        loadingbarView.image = animatedImage
+        loadingbarView.layer.cornerRadius = 10
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
